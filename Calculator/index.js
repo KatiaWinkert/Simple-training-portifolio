08d0e2dee124a817f83458a0b54d89b8ec041b18
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-08-13 22:00:34 -0300

    :rocket: Index JS
    Estrutura javascript onde faz a operação de calculo na calculadora atraves dos eventos acionados clicados nos inputs da estrutura.

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,5 +1,5 @@
 const switchBtn = document.getElementById("switch");
-const displayView = document.getElementById("display");
+const display = document.getElementById("display");
 const input = document.querySelectorAll("input");
 
 let value = "";
@@ -10,3 +10,28 @@ switchBtn.addEventListener("click", () =>{
         document.body.setAttribute("data-theme", "")
     }
 });
+input.forEach((e)=>
+e.addEventListener("click", (event)=>{
+    if(event.target.value == "="){
+        if(value.length != 0){
+            let newval = eval(value);
+            value = newval; 
+            display.value = value;
+        }
+    }else if(event.target.value == "c"){
+        value = ""; 
+        display.value = value; 
+    } else if(event.target.value == "switch"){
+    }else{
+        value += event.target.value;
+        display.value = value;
+    }
+
+    if(!event.target.classList.contains("stwich")){
+        event.target.classList.add("active"); 
+        setTimeout(()=>{
+            event.target.classList.remove("active");
+        }, 400)
+    }
+})
+)
